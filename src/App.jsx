a53b5570e38162fb81f2c13388a26eeb54cb2235
emikalie <euk304@psu.edu>
2025-12-05 13:20:37 -0500

    new stuff pushing

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import "./App.css";
 
 export default function App() {
     return (
-        <BrowserRouter basename="/reactSite">
+        <BrowserRouter basename="/reactCake">
             <div className="app-root">
                 <nav>
                     <Link to="/">Home</Link>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/reactSite/",
+    base: "/reactCake/",
     assetsInclude: ["**/*.jpg", "**/*.jpeg", "**/*.png", "**/*.gif"], // Add this line
 });
